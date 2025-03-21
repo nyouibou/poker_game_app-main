7b24f4a2b02dd691cad3ac1249fe8ed8b01da954
nyouibou <akshaits4@gmail.com>
2025-03-21 13:52:49 +0530

    updated webview with content viewing and access

---
+++ lib/widget/gamebrowser.dart
@@ -0,0 +1,24 @@
+// import 'package:flutter/material.dart';
+// import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+
+// class GameBrowser extends InAppBrowser {
+//   @override
+//   Future<void> onBrowserCreated() async {
+//     debugPrint("✅ Browser Created!");
+//   }
+
+//   @override
+//   void onLoadStart(WebUri? url) {
+//     debugPrint("🔄 Loading: ${url?.toString()}");
+//   }
+
+//   @override
+//   void onLoadStop(WebUri? url) {
+//     debugPrint("✅ Finished Loading: ${url?.toString()}");
+//   }
+
+//   @override
+//   void onExit() {
+//     debugPrint("🚪 Browser Closed.");
+//   }
+// }
